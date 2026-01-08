8e3ffbddade64a5b9e9a6e7c5fc8cda2a8df181d
shashank317 <s2770184@gmail.com>
2026-01-08 13:38:55 +0530

    yes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Configurator from "./pages/Configurator";
 
