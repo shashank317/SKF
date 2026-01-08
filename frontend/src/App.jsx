7a34b2eef32d4220ceee42314aef245ea57c733a
shashank317 <s2770184@gmail.com>
2026-01-08 13:37:11 +0530

    haa more changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home.jsx";
 import NotFound from "./pages/NotFound";
 import Configurator from "./pages/Configurator";
 
